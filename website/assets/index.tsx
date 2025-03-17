9dc755f35b6557ce0eba255042f8c5f396da3889
Shuyu Li <lsy27214@gmail.com>
2025-03-17 21:46:34 +1100

    1. Add sunscreenCalculator route 2. Create Sunscreen Calculator component.

--- website/assets/index.tsx
+++ website/assets/index.tsx
@@ -1,9 +1,24 @@
 import hero from "./hero.jpg";
 
 const navItems = [
-  { label: "UV Radiation", tabs: [{label: 'UV on Map', link:'/uvRadiation/uvMap'}] },
-  { label: "Impacts of UV exposure" },
-  { label: "UV Protection", tabs: [{label: 'Sunscreen Reminder', link:'/uvProtection/sunscreenReminder'}] },
-  { label: "Sun Protection products" },
+    {
+        label: "UV Radiation",
+        tabs: [{ label: "UV on Map", link: "/uvRadiation/uvMap" }],
+    },
+    { label: "Impacts of UV exposure" },
+    {
+        label: "UV Protection",
+        tabs: [
+            {
+                label: "Sunscreen Reminder",
+                link: "/uvProtection/sunscreenReminder",
+            },
+            {
+                label: "Sunscreen Calculator",
+                link: "/uvProtection/sunscreenCalculator",
+            },
+        ],
+    },
+    { label: "Sun Protection products" },
 ];
 export { hero, navItems };
